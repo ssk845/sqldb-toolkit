f23b4d0bf6de3a4cdf98d47deaebd5ee903133c6
Saad Kanwar <saad.kanwar@equisoft.com>
2021-11-27 16:14:31 -0500

    Added script to deploy masked bacpac file.

--- Maskedbacpac/deploymaskedbacpac.ps1
+++ Maskedbacpac/deploymaskedbacpac.ps1
@@ -1,1 +1,10 @@
-Publish-DbaDacPackage -SqlInstance sql2016 -Database DB1 -DacOption $options -Path c:\temp\db.dacpac
+# Get input values
+$SQLInstance = Read-Host "Input SQL Instance" 
+$SDatabase = Read-Host "Input Source Database Name" 
+$TDatabase = Read-Host "Input Target Database Name" 
+$Path = Read-Host "Input Path to Get Extract"
+# Get sql login from keyvault
+$sqladmin = 'sqlprb'
+$secretdetail = Get-AzKeyVaultSecret -VaultName $vault -Name $sqladmin
+$sqlcredential = New-Object System.Management.Automation.PSCredential ($secretdetail.Name, $secretdetail.SecretValue)
+Publish-DbaDacPackage -SqlInstance $SQLInstance  -SqlCredential $sqlcredential -Database $TDatabase -Path $Path'\'$SDatabase'.bacpac' -confirm:$false
