51927051c027d876699508d9ee9701a4fcebb62a
Saad Kanwar <saad.kanwar@equisoft.com>
2021-11-28 11:29:38 -0500

    Added Path.

---
+++ Reporting/auditentriessql.ps1
@@ -0,0 +1,23 @@
+# Get input values
+$SQLInstance = Read-Host "Input SQL Instance" 
+$StartAudit = Read-Host "Input Start Audit Date"
+$EndAudit = Read-Host "Input End Audit Date"
+$Path = Read-Host "Input Path Generate Audit File" 
+# Get sql login from keyvault
+$sqladmin = 'sqlprb'
+$secretdetail = Get-AzKeyVaultSecret -VaultName $vault -Name $sqladmin
+$sqlcredential = New-Object System.Management.Automation.PSCredential ($secretdetail.Name, $secretdetail.SecretValue)
+$output = Invoke-DbaQuery -SqlInstance $SQLInstance  -SqlCredential $sqlcredential -Query "SELECT 
+event_time,
+action_id,
+succeeded,
+server_principal_name,
+database_name,
+schema_name,
+object_name,
+statement,
+host_name
+FROM sys.fn_get_audit_file ( 'C:\audit\*.sqlaudit' , DEFAULT , DEFAULT)
+where action_id not in ('AUSC')
+and cast(event_time as date) between N'$StartAudit' and N'$EndAudit'"
+$output | Export-Csv -Path $Path\output.csv -NoTypeInformation
