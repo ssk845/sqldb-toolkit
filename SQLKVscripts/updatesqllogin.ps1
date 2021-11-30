e1351652fc563e3053393958af697e07345ceec3
Saad Kanwar <saad.kanwar@equisoft.com>
2021-11-30 13:36:19 -0500

    Fixed update script.

--- SQLKVscripts/updatesqllogin.ps1
+++ SQLKVscripts/updatesqllogin.ps1
@@ -4,6 +4,7 @@ Connect-AzAccount
 $serverName = Read-Host "Input servername" 
 $sqllogin = Read-Host "Input SQL login" 
 # Get sql login from keyvault
+$vault = 'toolscreds'
 $sqladmin = 'sqlprb'
 $secretdetail = Get-AzKeyVaultSecret -VaultName $vault -Name $sqladmin
 $sqlcredential = New-Object System.Management.Automation.PSCredential ($secretdetail.Name, $secretdetail.SecretValue)
@@ -15,14 +16,12 @@ $password = [System.Web.Security.Membership]::GeneratePassword(50, 1)
 $pattern = '[^a-zA-Z0-9#$!]' 
 # Trim password ~ length = 16 
 $password = ($password -replace $pattern, '').Substring(1, 16)
-# define variables
-$key1 = $sqllogin
-$vault = 'toolscreds'
-$secret1 = $password
 # Update secert with the new password
 Write-Host "`nUpdating password for login - $key1" 
+$secret1 = $password
 $value = ConvertTo-SecureString -String $secret1 -AsPlainText -Force 
 Set-AzKeyVaultSecret -VaultName $vault -Name $key1 -SecretValue $value 
-#convert password to an encrypted string
+#convert password to an encrypted string and define variables
+$key1 = $sqllogin
 $securepassword = ConvertTo-SecureString  $password -asplaintext -force
 Set-DbaLogin -SqlInstance $serverName -SqlCredential $sqlcredential -Login $sqllogin -SecurePassword $securepassword -Confirm:$false
