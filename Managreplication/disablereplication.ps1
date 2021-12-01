91de01826ad5239c489b5012e2a12320a9aa4e9b
Saad Kanwar <saad.kanwar@equisoft.com>
2021-11-30 19:23:36 -0500

    Added enable and disable replication scripts.

---
+++ Managreplication/disablereplication.ps1
@@ -0,0 +1,17 @@
+# Connect to Azure
+Connect-AzAccount
+# Get input
+$SqlInstance = Read-Host "Input SQL Instance" 
+$PublisherName = 'TestPub'
+# Get sql login from keyvault
+$vault = 'toolscreds'
+$sqladmin = 'sqladmin'
+$secretdetail = Get-AzKeyVaultSecret -VaultName $vault -Name $sqladmin
+$sqlcredential = New-Object System.Management.Automation.PSCredential ($secretdetail.Name, $secretdetail.SecretValue)
+#Stop and disable replication related 
+Stop-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-$PublisherName-1
+Stop-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-$PublisherName-$SqlInstance-3 
+Stop-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-1
+Set-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-1 -Disabled
+Set-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-$PublisherName-$SqlInstance-3 -Disabled
+Set-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-$PublisherName-1 -Disabled
---
+++ Managreplication/enablereplication.ps1
@@ -0,0 +1,16 @@
+# Connect to Azure
+Connect-AzAccount
+# Get input
+$SqlInstance = Read-Host "Input SQL Instance" 
+$PublisherName = 'TestPub'
+# Get sql login from keyvault
+$vault = 'toolscreds'
+$sqladmin = 'sqladmin'
+$secretdetail = Get-AzKeyVaultSecret -VaultName $vault -Name $sqladmin
+$sqlcredential = New-Object System.Management.Automation.PSCredential ($secretdetail.Name, $secretdetail.SecretValue)
+#Stop and disable replication related 
+Set-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-1 -Enabled
+Set-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-$PublisherName-$SqlInstance-3 -Enabled
+Set-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-$PublisherName-1 -Enabled
+# start Sanpshot SQL Job 
+Start-DbaAgentJob -SqlInstance $SqlInstance -SqlCredential $sqlcredential -Job $SqlInstance-AdventureWorksLT2019-$PublisherName-1
